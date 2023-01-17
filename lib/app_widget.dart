2759918b77f6a5e55802eda3f1f2ca268d147c06
NovatronicaST-DEV <emanuelngola33@gmail.com>
2023-01-17 11:59:21 +0100

    colum

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/app_controller.dart';
 import 'home_page.dart';
 
 class Appwidget extends StatelessWidget {
   @override
 
   Widget build(BuildContext context) {
-    
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
+    return AnimatedBuilder(animation: AppController.instance, builder:(context, child) {
+       return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red,
+      brightness: AppController.instance.isDartTheme? Brightness.light:Brightness.dark
+      ),
       home: HomePage(),
     );
+    });
+    
+   
   } 
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,46 +1,53 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/app_controller.dart';
 
 class HomePage extends StatefulWidget {
-State<HomePage> createState(){
-return HomePageState();
-}
-
+  State<HomePage> createState() {
+    return HomePageState();
+  }
 }
 
 class HomePageState extends State<HomePage> {
-  int counter=0;
-  Widget build(BuildContext context){
-   return Scaffold(
-    appBar: AppBar(
-      title: Text('Página principal'),
-      
-    ),
-   
-  body: Container(
-    height: 200,
-    width: 200,
-    color: Colors.black,
-    child: Align(
-      alignment: Alignment.center,
-      child: Container(
-        height: 100,
-        width: 100,
-        color: Colors.green,
+  int counter = 0;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Página principal'),
+        actions: [CustomSwitch(),],
       ),
-
-  
-    ),
-  ),
-     floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.add),
-      onPressed: () {
-       setState(() {
-          counter++;
-      });
-    
-    },),
-  );
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Contador:$counter'),
+            CustomSwitch()
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
   }
+}
+class CustomSwitch extends StatelessWidget {
+  const CustomSwitch({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+          value: AppController.instance.isDartTheme,
+          onChanged: (value) {
+            AppController.instance.changeTheme();
+          },
+        );
+  }
 }
